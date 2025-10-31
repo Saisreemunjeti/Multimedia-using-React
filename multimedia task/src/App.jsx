f2793679ca31ffe0f3a0194541c536cc40d0bcec
Jahnavi Munjeti <saisreemunjeti@gmail.com>
2025-10-31 13:49:19 +0530

    react application

---
+++ multimedia task/src/App.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <center>
+      <h1>🎬 Multimedia App</h1>
+      <h1><i>BUCKY THE BRAVE BUNNY
+        </i></h1>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/c/c5/Big_buck_bunny_poster_big.jpg"
+        alt="Example"
+        width="900"
+        height="1100"
+      />
+
+      <br /><br />
+      <div className="container2"><p>In a serene forest, there lived a colossal, gentle rabbit named Big Buck Bunny. His days were filled with joy, basking in the sun, 
+        watching butterflies flutter by, and enjoying the peaceful sounds of nature.</p>
+      <div className="container3"><img src="https://upload.wikimedia.org/wikipedia/commons/2/22/Big.Buck.Bunny.-.Bunny.Portrait.png" alt="" width="200"/></div>
+      </div>
+
+      <br /><br />
+       <div className="container3"><img src="https://peach.blender.org/wp-content/uploads/bbb-splash.png" alt="" width="400"/>
+       <div className="container2"><p>One day, while admiring a butterfly, three mischievous rodents—Frank the flying squirrel, Rinky the red squirrel, and Gimera the chinchilla—began 
+        causing trouble. They hurled fruits, nuts, and rocks at the 
+        defenseless creatures of the forest. The final straw came when they killed two butterflies with an apple and a rock, and then attacked Bunny himself.</p></div>
+       </div>
+
+       <br /><br /><br />
+       <div className="container">
+        <img src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg" alt="" width="500"/>
+       <p>Heartbroken and enraged, Bunny decided it was time to take action. He meticulously devised a series of clever traps to teach the bullies a lesson they wouldn't forget.
+        The plan was set into motion. One by one, Rinky and Gimera fell into Bunny's traps, incapacitated and humiliated. Frank, unaware of his companions' fate, soared through the air, only to trigger Bunny's final trap. 
+        He plummeted into a spike-laden pit, grabbing onto what he thought was a tree branch. To his horror, it was merely a twig held by Bunny, who then captured him.</p>
+        </div>
+        <br />
+      <p>As a final act of retribution, Bunny attached Frank to a string, transforming him into a kite, and let the wind carry him away. The forest erupted in cheers, 
+        celebrating the return of peace and justice.</p>
+      <video width="800" controls>
+        <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <br /><br />
+      <h1><i>THE END</i></h1>
+      </center>
+    </div>
+  );
+}
+
+export default App;
